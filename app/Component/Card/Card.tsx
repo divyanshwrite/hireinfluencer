2d152f185233d1e45c042ac0cf87c159f04d4be7
DivyanshWriteCode <divyanshdocode@gmail.com>
2025-02-18 17:55:02 +0530

    NewCard

--- app/Component/Card/Card.tsx
+++ app/Component/Card/Card.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const Card = ({title,description}) => {
-  return (
-    <div>
-        <div className='Card rounded-lg border bg-card text-card-foreground shadow-sm'>
-            <div className='p-6 pt-6'>
-                <i className='bx bx-search-alt-2 text-5xl'></i>
-                <h2 className="font-bold mb-2">{title}</h2>
-                <p className="text-sm text-muted-foreground">{description}</p>
-            </div>
-        </div>
-    </div>
-  )
+interface CardProps {
+  title: string;
+  description: string;
 }
 
-export default Card
+const Card: React.FC<CardProps> = ({ title, description }) => {
+  return (
+    <div>
+      <div className='Card rounded-lg border bg-card text-card-foreground shadow-sm'>
+        <div className='p-6 pt-6'>
+          <i className='bx bx-search-alt-2 text-5xl'></i>
+          <h2 className="font-bold mb-2">{title}</h2>
+          <p className="text-sm text-muted-foreground">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
